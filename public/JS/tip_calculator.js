94fc4766132ee986da3de30394ffa5fb6c4de9f6
Vivian Canales <viviancans@gmail.com>
2017-06-29 14:14:48 -0500

    Started writing tip calculator function

--- public/JS/tip_calculator.js
+++ public/JS/tip_calculator.js
@@ -1,3 +1,25 @@
 /*-----------------------------------------------------------
 		Chapter 1 Exercise - Calculator Tip Problem 
 -----------------------------------------------------------*/
+var billTotal = prompt("What is the total of your bill?");
+var tipPercentage = prompt("What is your tip percentage?");
+
+function tipCalculator(){
+	if (billTotal || tipPercentage == NaN){
+		alert("Please enter a valid number!");
+	}else{
+		var tipAmount = 0;
+	
+		tipAmount = billTotal * (tipPercentage/100);
+		billTotal = parseFloat(billTotal) + parseFloat(tipAmount);
+	
+		return "Tip Amount: " + tipAmount + " Bill Total: " + billTotal;
+		
+		}
+
+}
+
+
+
+
+console.log(tipCalculator());
